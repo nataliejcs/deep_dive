11698d010cf9f0d6ceb060b3925017df28b7708a
Arina Afanasjeva <arin0181@stud.kea.dk>
2021-10-05 13:22:44 +0200

    js

--- myjs.js
+++ myjs.js
@@ -2,26 +2,24 @@ const url = "https://kea21-b430.restdb.io/rest/new-collection";
 
 // The API-key
 const options = {
-  header: {
+  headers: {
     "x-apikey": "6136570f43cedb6d1f97ed67",
   },
 };
 
+fetch(url, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
 
-
-fetch(url,options)
-.then((response)=>{​
-if (!response.ok) {​
-throw Error(response.statusText);
-}​
-return response.json();
-}​)
-.then((data)=> {
-    // We have the data
-    console.log(data);
+  .then((data) => {
+    //we have the data
+    // console.log(data);
     // handleData(data);
-})
-.catch((e)=> {
-    console.error("An error occured", e.message);
-});
+    console.log(data);
+  });
 
+// function handleData(dogs) {}
